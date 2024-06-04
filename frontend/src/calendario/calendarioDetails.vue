62971f25e6250824afff4dee263e23f688cfede6
TereSolano01 <tere.solano.solis@est.una.ac.cr>
2024-06-04 15:15:45 -0600

    details works

--- frontend/src/calendario/calendarioDetails.vue
+++ frontend/src/calendario/calendarioDetails.vue
@@ -1,84 +1,129 @@
-<!-- professorDetails.vue -->
 <template>
   <div class="row">
-   <div class="eleven column" style="margin-top: 5%">
-    <h2>{{title}}</h2>
-     <form>
-     <div class="row">
-      <div class="six columns">
-    <label for="nameInput">Titulo</label>
-    <input class="u-full-width" type="text" v-model="calendar.titulo">
-  </div>
-  <div class="six columns">
-    <label for="degreeInput">Descripcion</label>
-    <input class="u-full-width" type="text" v-model="calendar.descripcion">
-  </div>
-</div>
-<div class="row">
-  <div class="six columns">
-    <label for="emailInput">Dia</label>
-    <input class="u-full-width" type="text" v-model="calendar.dia">
-  </div>
-  <div class="six columns">
-    <label for="phoneInput">Mes</label>
-    <input class="u-full-width" type="text" v-model="calendar.mes">
-  </div>
-      <router-link class="button button-primary" 
-        to="/calendario">Back</router-link>
-      <a v-if='edit' class="button button-primary" style="float: right"
-         v-on:click="updateCalendario()">Update</a>
-      <a v-if='create' class="button button-primary" style="float: right"
-         v-on:click="createCalendario()">Create</a>
-     </div>
-    </form>
-   </div>
+    <div class="eleven column" style="margin-top: 5%">
+      <h2>{{ title }}</h2>
+      <form>
+        <div class="row">
+          <div class="six columns">
+            <label for="tituloInput">Titulo</label>
+            <input class="u-full-width" type="text" v-model="calendar.titulo">
+          </div>
+        </div>
+        <div class="row">
+          <div class="twelve columns">
+            <label for="descripcionInput">Descripcion</label>
+            <input class="u-full-width" type="text" v-model="calendar.descripcion">
+          </div>
+        </div>
+        <div class="row">
+          <div class="six columns">
+            <label for="fechaInput">Fecha</label>
+            <input class="u-full-width" type="date" v-model="fecha" @input="updateFecha">
+          </div>
+          <div class="six columns">
+            <label for="horaInicioInput">Hora de Inicio</label>
+            <input class="u-full-width" type="time" v-model="calendar.hora_inicio">
+          </div>
+        </div>
+        <div class="row">
+          <div class="six columns">
+            <label for="horaFinInput">Hora de Fin</label>
+            <input class="u-full-width" type="time" v-model="calendar.hora_fin">
+          </div>
+          <div class="six columns">
+            <label for="ubicacionInput">Ubicación</label>
+            <input class="u-full-width" type="text" v-model="calendar.ubicacion">
+          </div>
+        </div>
+        <router-link class="button button-primary" to="/calendario">Back</router-link>
+        <a v-if='edit' class="button button-primary" style="float: right" v-on:click="updateCalendario()">Update</a>
+        <a v-if='create' class="button button-primary" style="float: right" v-on:click="createCalendario()">Create</a>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  props: ['create','edit','show'],
+  props: ['create', 'edit', 'show'],
   data: function() {
     return {
       title: "Reuniones data",
-      calend: {'id':'','titulo':'','descripcion':'','dia':'','mes':'','anio':'','hora_inicio':'','hora_fin':'','ubicacion':'','user_id':''},
+      calendar: {
+        id: '',
+        titulo: '',
+        descripcion: '',
+        dia: '',
+        mes: '',
+        anio: '',
+        hora_inicio: '',
+        hora_fin: '',
+        ubicacion: '',
+        user_id: ''
+      },
+      fecha: ''
     }
   },
-  created () {
-   this.findCalendario(app.$route.params.id)
+  created() {
+    if (this.$route.params.id) {
+      this.fetchCalendario(this.$route.params.id);
+    }
+  },
+  watch: {
+    'calendar.dia': 'updateFechaModel',
+    'calendar.mes': 'updateFechaModel',
+    'calendar.anio': 'updateFechaModel'
   },
   methods: {
-    findCalendario: function(id) {
-      var calend = app.calendarios.find(
-        function(x) { return x.id == id; });
-      if (calend!=null)
-        Object.assign(this.calend,calend);
+    fetchCalendario: function(id) {
+      axios.get(`https://meetingscalendar.000webhostapp.com/server/calendario/${id}`)
+        .then(response => {
+          console.log('Datos recibidos:', response.data);
+          if (Array.isArray(response.data) && response.data.length > 0) {
+            this.calendar = response.data[0];
+            this.fecha = `${this.calendar.anio}-${String(this.calendar.mes).padStart(2, '0')}-${String(this.calendar.dia).padStart(2, '0')}`;
+          }
+        })
+        .catch(error => {
+          console.error('There was an error fetching the calendario!', error);
+        });
     },
     updateCalendario: function() {
-      this.calend['_method'] = 'PUT';
-      var id = app.$route.params.id;
-      fetch('/server/calendario/'+id,
-        { headers: {'Content-Type':'application/json'},
-          method: 'POST',
-          body: JSON.stringify(this.calend)})
-        .then((data) => {
-          var index = app.calendarios.findIndex(
-             function(x) { return x.id == id; });
-          app.calendarios[index] = this.calend;
-          router.push('/calendario');
-        }
-      )
+      this.calendar['_method'] = 'PUT';
+      var id = this.$route.params.id;
+      axios.post(`https://meetingscalendar.000webhostapp.com/server/calendario/${id}`, this.calendar, {
+        headers: { 'Content-Type': 'application/json' }
+      })
+        .then(() => {
+          this.$router.push('/calendario');
+        })
+        .catch(error => {
+          console.error('There was an error updating the calendario!', error);
+        });
     },
     createCalendario: function() {
-      fetch('/server/calendario',
-        { headers: {'Content-Type':'application/json'},
-          method: 'POST',
-          body: JSON.stringify(this.calend)})
-        .then((data) => {
-           app.calendarios.push(this.calend);
-           router.push('/profecalendariossor');
-        }
-      )
+      axios.post('https://meetingscalendar.000webhostapp.com/server/calendario', this.calendar, {
+        headers: { 'Content-Type': 'application/json' }
+      })
+        .then(() => {
+          this.$router.push('/calendario');
+        })
+        .catch(error => {
+          console.error('There was an error creating the calendario!', error);
+        });
+    },
+    updateFecha() {
+      const [anio, mes, dia] = this.fecha.split('-');
+      this.calendar.anio = anio;
+      this.calendar.mes = mes;
+      this.calendar.dia = dia;
+    },
+    updateFechaModel() {
+      if (this.calendar.anio && this.calendar.mes && this.calendar.dia) {
+        this.fecha = `${this.calendar.anio}-${String(this.calendar.mes).padStart(2, '0')}-${String(this.calendar.dia).padStart(2, '0')}`;
+      }
     }
   }
 }
